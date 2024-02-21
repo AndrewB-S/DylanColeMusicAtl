205cc33bea7743b14c02a80b0c94c3216ec4585f
Andrew Sneed <ndrwsnd@gmail.com>
2024-02-21 17:59:20 -0500

    implemented decap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,30 +9,5 @@ export default defineConfig({
     applyBaseStyles:false
   }), 
     react(),
-    NetlifyCMS({
-      config: {
-        disableIdentityWidgetInjection: true,
-        backend: {
-          name: "git-gateway",
-          branch: "main",
-        },
-        media_folder: "src/assets/images/",
-        public_folder: "_astro/",
-        collections: [
-          {
-            name: "performances",
-            label: "Upcoming Performances",
-            folder: "src/content/performances",
-            create: true,
-            delete: true,
-            fields: [
-              { name: "title", widget: "string", label: "Title of Performance" },
-              {name: "date", widget: "datetime", label: "Date of Performance" },
-              { name: "body", widget: "markdown", label: "Description" },
-            ],
-          },
-        ],
-      },
-    }),
   ]
 });
