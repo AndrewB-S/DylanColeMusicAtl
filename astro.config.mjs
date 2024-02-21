2dee47a6d2d78fe5399e3a26ef7b1efb886ceeee
Andrew Sneed <ndrwsnd@gmail.com>
2024-02-21 17:15:49 -0500

    Integrating and testing DecapCMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +9,31 @@ export default defineConfig({
     tailwind({
     applyBaseStyles:false
   }), 
-    react()
+    react(),
+    NetlifyCMS({
+      config: {
+        disableIdentityWidgetInjection: true,
+        backend: {
+          name: "git-gateway",
+          branch: "main",
+        },
+        media_folder: "src/assets/images/",
+        public_folder: "_astro/",
+        collections: [
+          {
+            name: "performances",
+            label: "Upcoming Performances",
+            folder: "src/content/performances",
+            create: true,
+            delete: true,
+            fields: [
+              { name: "title", widget: "string", label: "Title of Performance" },
+              {name: "date", widget: "datetime", label: "Date of Performance" },
+              { name: "body", widget: "markdown", label: "Description" },
+            ],
+          },
+        ],
+      },
+    }),
   ]
 });
---
+++ src/content/performances/relics1023.md
@@ -0,0 +1,6 @@
+---
+title: Something
+date: 10-20-1029
+---
+
+Promotional Description of the performance if desired.
